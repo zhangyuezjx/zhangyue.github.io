1c395773742881852f6748793c3e42bce8db1c02
knightyun <2386209384@qq.com>
2019-03-02 15:47:36 +0800

    add /feed.xml

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,2 +1,11 @@
 var searchBtn = document.querySelector('.search-icon');
 var searInput = document.querySelector('.search-input');
+var xhr = new XMLHttpRequest() || new ActiveXObject('Microsoft.XMLHTTP');
+xhr.onreadystatechange = function(){
+    if (xhr.readyState == 4 && xhr.status == 200) {
+        xml = xhr.responseXML;
+        console.log(xml);
+    }
+}
+xhr.open('get', '/feed.xml', true);
+xhr.send();
