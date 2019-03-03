76b3fe0536372850f1bbad4f745c2d48d2e30fc9
knightyun <2386209384@qq.com>
2019-03-03 12:15:54 +0800

    hide search div

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,11 +1,76 @@
 var searchBtn = document.querySelector('.search-icon');
-var searInput = document.querySelector('.search-input');
+var searchInput = document.querySelector('.search-input');
+var searchResults = document.querySelector('.search-results');
+var searchValue = '',
+    arrItems = [],
+    arrContents = [],
+    arrLinks = [],
+    arrTitles = [],
+    arrResults = [];
+    indexItem = [];
+var tmpDiv = document.createElement('div');
+tmpDiv.className = 'result-item';
+
 var xhr = new XMLHttpRequest() || new ActiveXObject('Microsoft.XMLHTTP');
 xhr.onreadystatechange = function(){
     if (xhr.readyState == 4 && xhr.status == 200) {
         xml = xhr.responseXML;
-        console.log(xml);
+        arrItems = xml.getElementsByTagName('item');
+        for (i = 0; i < arrItems.length; i++) {
+            arrContents[i] = arrItems[i].getElementsByTagName('description')[0].childNodes[0].nodeValue;
+            arrLinks[i] = arrItems[i].getElementsByTagName('link')[0].childNodes[0].nodeValue.replace(/\s+/g, '');
+            arrTitles[i] = arrItems[i].getElementsByTagName('title')[0].childNodes[0].nodeValue;
+        }
     }
 }
 xhr.open('get', '/feed.xml', true);
 xhr.send();
+
+searchBtn.onclick = searchConfirm;
+searchInput.onkeydown = function(evt){
+    if (evt.keyCode == '13') searchConfirm();
+}
+
+function searchConfirm() {
+    if (searchInput.value == '' || searchInput.value.search(/^\s+$/) >= 0) {
+        // console.log(false);
+    } else {
+        console.log(true);
+        clearData();
+        searchValue = searchInput.value;
+        searchMatching(arrContents, searchValue);
+    }
+}
+
+function clearData() {
+    arrResults = [];
+    indexItem = [];
+    searchResults.innerHTML = '';
+}
+
+function searchMatching(matchArr, matchInput) {
+    for (i = 0; i < matchArr.length; i++) {
+        if (matchArr[i].search(matchInput) >= 0) {
+            indexItem.push(i);
+            var indexContent = matchArr[i].search(matchInput);
+            var l = matchInput.length;
+            arrResults.push(matchArr[0].slice(indexContent - 10, indexContent + l + 10));
+        }
+    }
+    if (indexItem.length == 0) {
+        console.log('no matching');
+        var itemDiv = tmpDiv.cloneNode(true);
+        itemDiv.innerText = 'no matching';
+        searchResults.appendChild(itemDiv);
+    }
+    for (i = 0, j = 0; i < arrResults.length, j < indexItem.length; i++, j++) {
+        var itemDiv = tmpDiv.cloneNode(true);
+        itemDiv.innerHTML = '<b>' + indexItem[i] + '</b> <br />' + arrResults[i];
+        itemDiv.setAttribute('onclick', 'changeHref(arrLinks[indexItem[' + i + ']])');
+        searchResults.appendChild(itemDiv);
+    }
+}
+
+function changeHref(href) {
+    location.href = href;
+}
--- projects.md
+++ projects.md
@@ -2,6 +2,6 @@
 layout: page
 title: "GitHub Projects"
 css: ["projects.css"]
-js: ["https://cdnjs.cloudflare.com/ajax/libs/geopattern/1.2.3/js/geopattern.min.js", "projects.js"]
+js: ["projects.js"]
 ---
 {% include projects.html %}
